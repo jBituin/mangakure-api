701932d799f00068d4f088e01e9bfffaddbc8651
Joemel Bituin <bituinjoemel@gmail.com>
2020-10-23 00:25:15 +0800

    Add chapterImage schema

--- src/manga/manga.module.ts
+++ src/manga/manga.module.ts
@@ -4,12 +4,14 @@ import { MangaService } from './manga.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MangaSchema } from './schemas/manga.schema';
 import { ChapterSchema } from './schemas/chapter.schema';
+import { ChapterImageSchema } from './schemas/chapterImage.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: 'Manga', schema: MangaSchema },
       { name: 'Chapter', schema: ChapterSchema },
+      { name: 'ChapterImage', schema: ChapterImageSchema },
     ]),
   ],
   controllers: [MangaController],
--- src/manga/schemas/chapterImage.schema.ts
+++ src/manga/schemas/chapterImage.schema.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-export const ChapterImage = new mongoose.Schema({
+export const ChapterImageSchema = new mongoose.Schema({
   chapterId: String,
   url: String,
   sequence: Number,
